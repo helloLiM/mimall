c0c31eb4b4d6df8a43bce20430a33a4bde6cab07
sam <1136019519@qq.com>
2020-05-14 22:54:08 +0800

    静态资源copy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="你好啊，我叫赛利亚"/>
   </div>
 </template>
 
